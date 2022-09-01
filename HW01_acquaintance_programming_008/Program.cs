f678227bd4b3910d7ffdad819019f0819523e4ed
Konstantin Kochetkov <kmkochetkov@gmail.com>
2022-09-02 00:22:15 +0300

    Домашнее задание. Задача 8

---
+++ HW01_acquaintance_programming_008/Program.cs
@@ -0,0 +1,13 @@
+﻿//Программа, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число N:");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write($"Чётные числа в диапазоне [1 - {number}] : ");
+
+for (int numberT=2; numberT<=number; numberT+=2)
+{    
+  Console.Write($" {numberT}");
+}
+Console.WriteLine();
